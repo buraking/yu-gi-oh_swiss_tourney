70609970c43c58ecdc39c2f25db0a311fd8f2755
Marco Almeida <marco.almeida@karnovgroup.com>
2018-03-17 17:26:17 +0100

    Add redirect to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     session[:current_user_id] = user.id
+    redirect_to tournaments_path
   end
 
   def toSignUp

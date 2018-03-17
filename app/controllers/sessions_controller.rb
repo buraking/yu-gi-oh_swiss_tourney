345596d76032b0d51af2ea1cc98ca9469d1bc2be
Marco Almeida <marco.almeida@karnovgroup.com>
2018-03-17 12:17:53 +0100

    Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    session[:current_user_id] = user.id
+  end
+end

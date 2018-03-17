140d83f8282928abdefd9e74219baba274958e29
Bo <bolacour@hotmail.com>
2018-03-17 18:22:47 +0100

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true,  format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: 'please'}
+  validates :email, 
+    presence: true,
+    uniqueness: true,
+    format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: 'Specify valid email'}
   has_many :matches
 end

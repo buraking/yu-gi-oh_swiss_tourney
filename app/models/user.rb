024e099f7ca80d752e721f46aa56c1aa18ed06b7
Søren <soer109a@stud.kea.dk>
2018-03-18 15:28:51 +0100

    added the profile the system and styled it up in an acceptable way.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
     format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: 'Specify valid email'}
   has_many :matches
   has_many :decks
+  has_many :participants
+  has_many :tournaments, through: :participants
 
   def gravatar_url
     "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=30&d=robohash"

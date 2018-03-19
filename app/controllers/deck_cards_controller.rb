9f252631287af5a7e9eff19117852f127818bca5
Marco Almeida <marco.almeida@karnovgroup.com>
2018-03-19 15:14:35 +0100

    Allow cards to be added in lowercase

--- app/controllers/deck_cards_controller.rb
+++ app/controllers/deck_cards_controller.rb
@@ -1,7 +1,7 @@
 class DeckCardsController < ApplicationController
   def create
     deck = Deck.find(params[:deck_id])
-    card = Card.where(number: params[:number]).first
+    card = Card.where(number: params[:number].upcase).first
     deck.cards.push(card)
 
     redirect_to deck
